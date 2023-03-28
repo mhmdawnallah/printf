22767046dfce701e4222b6b99bde3e914f57fe21
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2023-03-28 18:01:23 +0200

    Fix linting errors betty-code style in _printf program

--- _printf.c
+++ _printf.c
@@ -6,66 +6,65 @@
 /**
  * _printf - Function to _printf
  * @format: Pointer to format
- * 
+ *
  * Return: Integer, number of chars printed
  */
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    char c;
-    char *s;
-    int d;
-    va_list args;
-    va_start(args, format);
-    
-    while (*format != '\0')
-    {
-        if (*format == '%')
-        {
-            format++;
-            switch (*format)
-            {
-                case 'c':
-                {
-                    c = (char)va_arg(args, int);
-                    putchar(c);
-                    count++;
-                    break;
-                }
-                case 's':
-                {
-                    s = va_arg(args, char *);
-                    puts(s);
-                    count += strlen(s);
-                    break;
-                }
-                case 'd':
-                case 'i':
-                {
-                    d = va_arg(args, int);
-                    printf("%d", d);
-                    count++;
-                    break;
-                }
-                case '%':
-                {
-                    putchar('%');
-                    count++;
-                    break;
-                }
-                default:
-                {
-                    break;
-                }
-            }
-        }
-        else
-        {
-            putchar(*format);
-            count++;
-        }
-        format++;
-    }
-    va_end(args);
-    return (count);
+int count = 0;
+char c;
+char *s;
+int d;
+va_list args;
+va_start(args, format);
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+switch (*format)
+{
+case 'c':
+{
+c = (char)va_arg(args, int);
+putchar(c);
+count++;
+break;
+}
+case 's':
+{
+s = va_arg(args, char *);
+puts(s);
+count += strlen(s);
+break;
+}
+case 'd':
+case 'i':
+{
+d = va_arg(args, int);
+printf("%d", d);
+count++;
+break;
+}
+case '%':
+{
+putchar('%');
+count++;
+break;
+}
+default:
+{
+break;
+}
+}
+}
+else
+{
+putchar(*format);
+count++;
+}
+format++;
+}
+va_end(args);
+return (count);
 }
