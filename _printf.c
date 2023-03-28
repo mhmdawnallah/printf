e004123da628d02fba7ccc78569c67531788f81b
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2023-03-28 22:22:30 +0200

    Handle the case where format string is null

--- _printf.c
+++ _printf.c
@@ -132,7 +132,10 @@ int _printf(const char *format, ...)
 int num_chars = 0;
 va_list args;
 va_start(args, format);
-
+if (format == NULL)
+{
+return (-1);
+}
 while (*format)
 {
 if (*format == '%')
